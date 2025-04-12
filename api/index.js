b52c9cc2bcbe95e0f884231b3ec9ee0e2f41dc8d
Tomas Maldonis <tomasis7@gmail.com>
2025-04-12 10:10:43 +0200

    revert back

--- api/index.js
+++ api/index.js
@@ -1,61 +1,39 @@
-import { ApolloServer } from "apollo-server-express";
+import { ApolloServer, gql } from "apollo-server-express";
+import { ApolloServerPluginDrainHttpServer } from "apollo-server-core";
+import http from "http";
 import express from "express";
 import cors from "cors";
-import { gql } from "apollo-server-express";
 
-// Define schema
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+const httpServer = http.createServer(app);
+
 const typeDefs = gql`
   type Query {
     hello: String
   }
 `;
 
-// Define resolvers
 const resolvers = {
   Query: {
     hello: () => "world",
   },
 };
 
-// Server instance - will be reused between requests
-let apolloServer = null;
-
-// Initialize Apollo Server
-async function initializeApollo() {
-  if (!apolloServer) {
-    apolloServer = new ApolloServer({
-      typeDefs,
-      resolvers,
-      // Crucial for serverless:
-      introspection: true,
-    });
-    await apolloServer.start();
-  }
-  return apolloServer;
-}
-
-// Vercel serverless handler
-export default async function handler(req, res) {
-  // Setup express
-  const app = express();
-  app.use(cors());
-  app.use(express.json());
-
-  // Initialize Apollo
-  const server = await initializeApollo();
-
-  // Apply middleware to express
-  server.applyMiddleware({ app });
-
-  // Handle the request with express
-  return new Promise((resolve, reject) => {
-    const expressHandler = app._router.handle.bind(app._router);
-    expressHandler(req, res, (err) => {
-      if (err) {
-        reject(err);
-      } else {
-        resolve();
-      }
-    });
+const startApolloServer = async (app, httpServer) => {
+  const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+    plugins: [ApolloServerPluginDrainHttpServer({ httpServer })],
   });
-}
+
+  await server.start();
+  server.applyMiddleware({ app });
+};
+
+startApolloServer(app, httpServer);
+
+export default httpServer;
