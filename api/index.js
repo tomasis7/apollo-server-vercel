6b8369685b32e58b7ddf7281e228d5de7ca3ed5c
Tomas Maldonis <tomasis7@gmail.com>
2025-04-16 20:48:15 +0200

    Refactor Apollo Server initialization and middleware setup in index.js for improved clarity and performance

--- api/index.js
+++ api/index.js
@@ -1,37 +1,24 @@
-import { ApolloServer, gql } from "apollo-server-express";
 import express from "express";
 import cors from "cors";
+import { ApolloServer, gql } from "apollo-server-express";
 
-let apolloServer = null;
+const app = express();
+app.use(cors()), express.json();
+
+const typeDefs = gql`
+  type Query {
+    hello: String
+  }
+`;
+
+const resolvers = { Query: { hello: () => "world" } };
+
+const server = new ApolloServer({ typeDefs, resolvers, introspection: true });
+const serverStartup = server.start().then(() => {
+  server.applyMiddleware({ app, path: "/", cors: false });
+});
 
 export default async function handler(req, res) {
-  const app = express();
-  app.use(cors());
-  app.use(express.json());
-
-  if (!apolloServer) {
-    const typeDefs = gql`
-      type Query {
-        hello: String
-      }
-    `;
-
-    const resolvers = {
-      Query: {
-        hello: () => "world",
-      },
-    };
-
-    apolloServer = new ApolloServer({
-      typeDefs,
-      resolvers,
-      introspection: true,
-    });
-
-    await apolloServer.start();
-  }
-
-  apolloServer.applyMiddleware({ app, path: "/", cors: false });
-
+  await serverStartup;
   return app(req, res);
 }
